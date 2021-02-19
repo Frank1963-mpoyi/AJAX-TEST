1ce4359bf51f22496e4f26e24bd23fbf39c9beca
Mpoyi-Tshibuyi <mpoyitshibuyi63@gmail.com>
2021-02-19 07:21:28 +0200

    Ajax api test

---
+++ app.js
@@ -0,0 +1,44 @@
+
+
+let apiButton = document.querySelector('#api-btn')
+
+apiButton.addEventListener('click', function (){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://arthurfrost.qflo.co.za/php/getTimeline.php', true)
+    xhr.send();
+    xhr.onload =() => {
+        if (xhr.status === 200){
+            let time = xhr.responseText;      
+            let timeline = JSON.parse(time)
+            // console.log(timeline.Timeline[516])
+            displayData(timeline)
+        }
+
+    };
+})
+
+
+
+let displayData = (timeline) => {
+    let htmlTemplate = `<p>ID: ${timeline.Timeline[516].Id}</p>
+                        <p>TITLE: ${timeline.Timeline[516].Title}</p>
+                        <p>MEDIA: ${timeline.Timeline[516].Media}</p>
+                        <p>DESCRIPTION: ${timeline.Timeline[516].Description}</p>
+                        <p>IMAGE: ${timeline.Timeline[516].Image}</p>
+                        <p>ICON: ${timeline.Timeline[516].Icon}</p>
+                        <p>AUDIO: ${timeline.Timeline[516].Audio}</p>
+                        <p>REMOTE ID: ${timeline.Timeline[516].RemoteId}</p>
+                        <p>STATUS: ${timeline.Timeline[516].Status}</p>
+                        <p>IS ACTIVE: ${timeline.Timeline[516].isActive}</p>
+                        <p>IN ID: ${timeline.Timeline[516].inId}</p>
+                        <p>CREATE DATE: ${timeline.Timeline[516].CreateDate}</p>
+                        <p>MEDIA NAME: ${timeline.Timeline[516].MediaName}</p>
+                        <p>CATEGORY: ${timeline.Timeline[516].Category}</p>
+                        <p>EPOCH: ${timeline.Timeline[516].Epoch}</p>
+                        <p>AUDIO SIZE: ${timeline.Timeline[516].AudioSize}</p>
+    `;
+    document.querySelector('#api-card').innerHTML = htmlTemplate;
+};
+
+
+
